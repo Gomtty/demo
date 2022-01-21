5904477126866bffa9f53ba41fea818b2beb9c6f
Gomtty <gomttystart@gmail.com>
2022-01-21 10:43:02 +0800

    hot-fix test 2

--- src/main/java/com/myoi/exer/Dog.java
+++ src/main/java/com/myoi/exer/Dog.java
@@ -23,6 +23,7 @@ class DogTest{
     public static void main(String[] args) {
 
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test 2");
         Dog dog=new Dog(){
             @Override
             public void eat() {
